4c5f6af357c4efd6d82aac051a473ebbbad43e5e
11mat11 <maticraft13@gmail.com>
2026-09-07 14:15:41 +0200

    export to full typescript

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,11 @@
+import { ExpoConfig, ConfigContext } from 'expo/config';
+
 const googleMapsApiKey = process.env.EXPO_PUBLIC_GOOGLE_MAPS_ANDROID_API_KEY;
 
-module.exports = ({ config }) => ({
+export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
+  name: config.name || "trekipelago",
+  slug: config.slug || "trekipelago",
   android: {
     ...config.android,
     predictiveBackGestureEnabled: false,
@@ -16,6 +20,6 @@ module.exports = ({ config }) => ({
     'expo-font',
     'expo-image',
     'expo-sqlite',
-    ...(googleMapsApiKey ? [['react-native-maps', { androidGoogleMapsApiKey: googleMapsApiKey }]] : []),
+    ...(googleMapsApiKey ? [['react-native-maps', { androidGoogleMapsApiKey: googleMapsApiKey }] as [string, any]] : []),
   ],
 });
